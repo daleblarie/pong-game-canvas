ad14491a0b73a4af7213ec5c2acc25d41d4b3c56
Dale Larie <daleblarie@gmail.com>
2018-01-02 21:36:56 -0500

    the stuff we did today

--- pong-game-canvas.js
+++ pong-game-canvas.js
@@ -14,9 +14,26 @@ function Coord(x, y) {
 }
 
 function Velocity(xVel, yVel) {
-  this.xVel = xVel
-  this.yVel = yVel
+  this.xVel = xVel;
+  this.yVel = yVel;
 }
+
+function Score(Lscore, Rscore) {
+  this.Lscore = Lscore;
+  this.Rscore = Rscore;
+}
+Score.prototype.incrementRscore = function incrementRscore() {
+  this.Rscore += 1;
+};
+
+Score.prototype.incrementLscore = function incrementLscore() {
+  this.Lscore += 1;
+};
+
+Score.prototype.draw = function draw() {
+  $('#Lscore span').html(this.Lscore);
+  $('#Rscore span').html(this.Rscore);
+};
 
 // paddle class
 function Paddle(isLeft) {
@@ -56,34 +73,43 @@ Paddle.prototype.draw = function draw() {
 // ball class
 function Ball(lpaddle, rpaddle) {
   this.pos = new Coord(BOARD_WIDTH / 2, BOARD_HEIGHT / 2);
-  this.vel = new Velocity(1, 0);
+  this.vel = new Velocity(1, -1);
   this.height = 2 * BALL_RADIUS;
   this.width = 2 * BALL_RADIUS;
   this.lpaddle = lpaddle;
   this.rpaddle = rpaddle;
 }
 
+Ball.prototype.isInEndzone = function isInEndzone(score) {
+  if (this.ball.pos < 0) {
+    score.incrementRscore();
+  }
+  if (this.ball.pos > BOARD_WIDTH) {
+    score.incrementLscore();
+  }
+};
+
 Ball.prototype.move = function move() {
   // cases, change velocity if needed
-  if (this.pos.y === BALL_RADIUS || this.pos.y === BOARD_HEIGHT - BALL_RADIUS) {
+  // bounce off the top and bottom
+  if (this.pos.y < BALL_RADIUS || this.pos.y > BOARD_HEIGHT - BALL_RADIUS) {
     this.vel.yVel *= -1;
-  }  else if (this.pos.x === BALL_RADIUS + PADDLE_WIDTH && (lpaddle.topLeft.y < this.pos.y) && this.pos.y < (lpaddle.topLeft.y + PADDLE_HEIGHT)) {
+  } else if (this.pos.x === BALL_RADIUS + PADDLE_WIDTH && (this.lpaddle.topLeft.y < this.pos.y) && this.pos.y < (this.lpaddle.topLeft.y + PADDLE_HEIGHT)) {
     this.vel.xVel = 1;
-    this.vel.yVel = ((this.pos.y - lpaddle.topLeft.y) - (PADDLE_HEIGHT / 2)) / (PADDLE_HEIGHT / 2);
-  }  else if (this.pos.x === BOARD_WIDTH - (BALL_RADIUS + PADDLE_WIDTH) && (rpaddle.topLeft.y < this.pos.y) && this.pos.y < (rpaddle.topLeft.y + PADDLE_HEIGHT)) {
+    this.vel.yVel = ((this.pos.y - this.lpaddle.topLeft.y) - (PADDLE_HEIGHT / 2)) / (PADDLE_HEIGHT / 2);
+  } else if (this.pos.x === BOARD_WIDTH - (BALL_RADIUS + PADDLE_WIDTH) && (this.rpaddle.topLeft.y < this.pos.y) && this.pos.y < (this.rpaddle.topLeft.y + PADDLE_HEIGHT)) {
     this.vel.xVel = -1;
-    this.vel.yVel = ((this.pos.y - rpaddle.topLeft.y) - (PADDLE_HEIGHT / 2)) / (PADDLE_HEIGHT / 2);
+    this.vel.yVel = ((this.pos.y - this.rpaddle.topLeft.y) - (PADDLE_HEIGHT / 2)) / (PADDLE_HEIGHT / 2);
   }
 
 
   // actually move according to updated velocity
   this.pos.x = this.pos.x + this.vel.xVel;
   this.pos.y = this.pos.y + this.vel.yVel;
-  ball.draw();
 };
 
 Ball.prototype.draw = function draw() {
-  ctx.clearRect(PADDLE_WIDTH, 0, BOARD_WIDTH - PADDLE_WIDTH * 2, BOARD_HEIGHT);
+  ctx.clearRect(PADDLE_WIDTH, 0, BOARD_WIDTH - (PADDLE_WIDTH * 2), BOARD_HEIGHT);
   ctx.fillStyle = 'rgb(0, 200, 0)';
   ctx.beginPath();
   ctx.arc(this.pos.x, this.pos.y, BALL_RADIUS, 0, Math.PI * 2, 1);
@@ -92,13 +118,8 @@ Ball.prototype.draw = function draw() {
   ctx.closePath();
 };
 
-
-
-
-
-
-
 // actually run the shit
+const score = new Score(0, 0);
 const lpaddle = new Paddle(true);
 lpaddle.draw();
 const rpaddle = new Paddle(false);
@@ -127,15 +148,13 @@ setInterval(() => {
   lpaddle.draw();
   ball.move();
   ball.draw();
+  score.draw();
+  ball.isInEndzone(score);
 }, 15);
 
 $(document).keydown(handler);
 $(document).keyup(handler);
 
-// game loop like in regular pong game
-// maybe start making ball
-// ball.draw function
-// bounce off at paddle at angle
-// window.confirm stuff for after scoring
-
-window.confirm('Do you Things?');
+// restart with a score in the game loop
+// confirm score and restart
+// window.confirm('Do you Things?');
